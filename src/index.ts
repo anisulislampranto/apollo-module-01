739fa53783843db999ae84ea3e4381ae41a52a35
Anisul Islam <anisulislam.pranto@gmail.com>
2023-05-02 09:11:25 +0600

    primitive types in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,30 @@
 var course: string = 'Next developer course';
-console.log(course);
+course = '999';
+
+let rollNumber:number = 10;
+rollNumber = 20;
+
+const isMarried = true
+
+let ccourse;
+ccourse = 'next developer course'
+ccourse = 999
+ccourse = true
+
+let coursee:string;
+coursee =  'next developer course'
+
+
+let cccourse:undefined  
+
+let ccccourse = undefined
+ccccourse = 'hello'
+ccccourse = 999
+
+
+// Primitive types
+// string
+// number
+// boolean
+// null
+// undefined
